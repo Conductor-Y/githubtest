4b4ad341fed9a8f2c1f1a96cd76e2349ccfeb1d8
“Conductor-Y” <3243096973@qq.com>
2024-01-10 10:03:36 +0800

    first commit

---
+++ liblcthw/src/lcthw/list.c
@@ -0,0 +1,158 @@
+#include <lcthw/list.h>
+#include <lcthw/dbg.h>
+
+List *List_create()
+{
+    return calloc(1, sizeof(List));
+}
+
+void List_destroy(List *list)
+{
+    LIST_FOREACH(list, first, next, cur) 
+	{
+        if(cur->prev) 
+		{
+            free(cur->prev);
+        }
+    }
+
+    free(list->last);
+    free(list);
+}
+
+
+void List_clear(List *list)
+{
+    LIST_FOREACH(list, first, next, cur) 
+	{
+        free(cur->value);
+    }
+}
+
+
+void List_clear_destroy(List *list)
+{
+    List_clear(list);
+    List_destroy(list);
+}
+
+
+void List_push(List *list, void *value)
+{
+    ListNode *node = calloc(1, sizeof(ListNode));
+    check_mem(node);
+
+    node->value = value;
+
+    if(list->last == NULL) {
+        list->first = node;
+        list->last = node;
+    } else {
+        list->last->next = node;
+        node->prev = list->last;
+        list->last = node;
+    }
+
+    list->count++;
+
+error:
+    return;
+}
+
+void *List_pop(List *list)
+{
+    ListNode *node = list->last;
+    return node != NULL ? List_remove(list, node) : NULL;
+}
+
+void List_unshift(List *list, void *value)
+{
+    ListNode *node = calloc(1, sizeof(ListNode));
+    check_mem(node);
+
+    node->value = value;
+
+    if(list->first == NULL) {
+        list->first = node;
+        list->last = node;
+    } else {
+        node->next = list->first;
+        list->first->prev = node;
+        list->first = node;
+    }
+
+    list->count++;
+
+error:
+    return;
+}
+
+void *List_shift(List *list)
+{
+    ListNode *node = list->first;
+    return node != NULL ? List_remove(list, node) : NULL;
+}
+
+void *List_remove(List *list, ListNode *node)
+{
+    void *result = NULL;
+
+    check(list->first && list->last, "List is empty.");
+    check(node, "node can't be NULL");
+
+    if(node == list->first && node == list->last) {
+        list->first = NULL;
+        list->last = NULL;
+    } else if(node == list->first) {
+        list->first = node->next;
+        check(list->first != NULL, "Invalid list, somehow got a first that is NULL.");
+        list->first->prev = NULL;
+    } else if (node == list->last) {
+        list->last = node->prev;
+        check(list->last != NULL, "Invalid list, somehow got a next that is NULL.");
+        list->last->next = NULL;
+    } else {
+        ListNode *after = node->next;
+        ListNode *before = node->prev;
+        after->prev = before;
+        before->next = after;
+    }
+
+    list->count--;
+    result = node->value;
+    free(node);
+
+error:
+    return result;
+}
+
+void List_link(List *list, List *list_2)
+{
+
+	if(list_2->first != NULL)		//check the last list is not empty
+	{
+		if(list->last != NULL)		//if the first list is not empty
+		{
+			list->last->next = list_2->first;
+			list_2->first->prev = list->last;
+			list->count = list->count + list_2->count;
+		}
+		else		//if the first list is empty
+		{
+			list->first = list_2->first;
+		}	
+
+		list->last = list_2->last;
+	}
+}
+
+void List_copy(List *list, List *list_2)
+{
+	while(list->count != 0)
+	{
+		char *val = List_shift(list);
+		List_push(list_2, val);
+	}
+
+}
+
